795905359385d7710f2d51a606fe4768673d031f
jacksonz <jacksonzhang34@gmail.com>
2025-06-26 18:16:28 -0500

    fix: removed gridAviary

--- gym_pybullet_drones/envs/__init__.py
+++ gym_pybullet_drones/envs/__init__.py
@@ -3,6 +3,5 @@ from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 from gym_pybullet_drones.envs.VelocityAviary import VelocityAviary
 from gym_pybullet_drones.envs.HoverAviary import HoverAviary
 from gym_pybullet_drones.envs.MultiHoverAviary import MultiHoverAviary
-from gym_pybullet_drones.envs.GridAviary import GridAviary
 
 __all__ = ['BaseAviary', 'CtrlAviary', 'VelocityAviary', 'HoverAviary', 'MultiHoverAviary', 'GridAviary']
